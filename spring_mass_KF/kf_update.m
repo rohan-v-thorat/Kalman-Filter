80d9ea4f3ff8d28fa2f491a6d096ceac501a2fd9
Rohan Thorat <rohanthorat2@gmail.com>
2023-04-16 17:02:06 +0530

    Update kf_update.m
    Did conceptual corrections

--- spring_mass_KF/kf_update.m
+++ spring_mass_KF/kf_update.m
@@ -1,9 +1,9 @@
-function [M,P] = kf_update(M,P,y,C,R)
+function [M,P] = kf_update(M,P,y,C,D,u,R)
 
   
   S = (C*P*C'+ R);
   K = P*C'/S;
-  M = M + K * (y-C*M);
+  M = M + K * (y-C*M-D*u);
   P = P - K*S*K';
   
 
